ddb93766ec7dee0896da7240121650aa308de3f0
Pietrek xD <pietrek.j@gmail.com>
2021-06-29 11:52:36 +0200

    vscode config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 CFG=".Xresources .bashrc .gitconfig .vimrc"
-cp -b $CFG ~/
+cp -sb $CFG ~/
 xrdb ~/.Xresources
 dconf load /apps/guake/ < ./guake.ini
+
+VSCODE_DIR="$HOME/.config/${VSCODE_PROFILE:-Code - OSS}/User"
+mkdir -p "$VSCODE_DIR" && cp -sb "$(pwd)/vscode-config"/*.json "$VSCODE_DIR/"
 
