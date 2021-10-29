f00d25bfb32c2faeb917b4b9b9d41548174ecd2d
Pietrek xD <pietrek.j@gmail.com>
2021-10-29 22:58:15 +0200

    fix cp

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ CFG=".Xresources .bashrc .gitconfig .vimrc"
 mkdir -p ~/.config/openbox/ ~/.config/lxpanel/LXDE/panels/
 
 cp -sb $CFG ~/
-cp -sb ./gui/lxde-rc.xml ~/.config/openbox/
-cp -sb ./gui/panel ~/.config/lxpanel/LXDE/panels/
-cp -sb ./gui/autostart ~/.config/lxsession/LXDE/
+cp -sb $(pwd)/gui/lxde-rc.xml ~/.config/openbox/
+cp -sb $(pwd)/gui/panel ~/.config/lxpanel/LXDE/panels/
+cp -sb $(pwd)/gui/autostart ~/.config/lxsession/LXDE/
 xrdb ~/.Xresources
 dconf load /apps/guake/ < ./gui/guake.ini
 
